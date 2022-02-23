5b008a998eeae50a9885e12f7a554308a83d7ce0
M1sterDonut <lhg@tuta.io>
2022-02-23 21:29:38 +0200

    Add files via upload

--- SQL_ORDERBY_LIMIT.sql
+++ SQL_ORDERBY_LIMIT.sql
@@ -16,3 +16,26 @@ SELECT * FROM payment
 WHERE amount != 0.00
 ORDER BY payment_date DESC
 LIMIT 5;
+
+
+--CHALLANGE
+
+--First 10 paying customers (payment IDs)
+SELECT customer_id,amount,payment_date FROM payment
+WHERE amount != 0.00
+ORDER BY payment_date ASC
+LIMIT 10;
+
+--5 shortest movies (runtime)
+SELECT title,length FROM film
+ORDER BY length ASC
+LIMIT 5;
+
+--all movies that are 50 min or shorter
+SELECT title,length FROM film
+WHERE length <= 50
+ORDER BY title ASC;
+
+--number of movies that are 50 min or shorter
+SELECT COUNT(title) FROM film
+WHERE length <= 50;
